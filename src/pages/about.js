e3dc461d1964463eb464ea2417af634c20be5a5e
Emil Lipskij <emil.lipskij@gmail.com>
2021-03-03 18:27:27 +0200

    Innitial commit
    * Add nav bar
    
    * Add about and projects page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function About() {
+  return (
+    <div>
+      <h1>About page</h1>
+      <p>I hate lorem ipsum so i'm not going to use it this time.</p>
+      <p>I hate lorem ipsum so i'm not going to use it this time.</p>
+      <p>I hate lorem ipsum so i'm not going to use it this time.</p>
+    </div>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <Navbar />
+      <div>
+        <h2>Train</h2>
+        <h3>Gain & Destroy</h3>
+        <p>Front-End web developer based in Vilius</p>
+      </div>
+    </section>
+  )
 }
