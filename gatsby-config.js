61d71fc1d4e9da8bbac8bdada075f763d11d3d8f
Emil Lipskij <emil.lipskij@gmail.com>
2021-03-04 13:53:39 +0200

    Add graphql query

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,4 +7,9 @@
 module.exports = {
   /* Your site config here */
   plugins: [],
+  siteMetadata: {
+    title: "One Fan",
+    description: "website for portfolio",
+    copyright: "This website is copyright 2021 One Fan",
+  },
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
-import { Link } from "gatsby"
+import { graphql, Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
-import { header, btn } from "../styles/home.module.css" 
+import { header, btn } from "../styles/home.module.css"
 
-export default function Home() {
+export default function Home({ data }) {
+  console.log(data)
+  // const { title, description } = data.site.siteMetadata
   return (
     <Layout>
       <section className={header}>
@@ -15,8 +17,19 @@ export default function Home() {
             My Portfolio Projects
           </Link>
         </div>
-        <img src='/opm.png' alt ='ok' style={{maxWidth: '100%'}}/>
+        <img src="/opm.png" alt="ok" style={{ maxWidth: "100%" }} />
       </section>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query SiteInfo {
+    site {
+      siteMetadata {
+        description
+        title
+      }
+    }
+  }
+`
