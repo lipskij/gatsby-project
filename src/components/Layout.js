e5181cac27b5679444c40adb6ee53b385245ea28
Emil Lipskij <emil.lipskij@gmail.com>
2021-03-04 13:29:23 +0200

    Add new favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,15 @@
 import React from "react"
+import favicon from "../../static/favicon.ico"
+import Helmet from "react-helmet"
 import Navbar from "./Navbar"
 import "../styles/global.css"
 
 export default function Layout({ children }) {
   return (
     <div className="layout">
+      <Helmet>
+        <link rel="icon" href={favicon} />
+      </Helmet>
       <Navbar />
       <div className="content">{children}</div>
       <footer>
